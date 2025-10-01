cfcba41301394b0979932b2db92136bcc660fd7a
Shi-Lan <XdllQs@gmail.com>
2025-10-01 06:29:31 +0200

    Update v8config.h

--- vendor/v8/9.3/include/v8config.h
+++ vendor/v8/9.3/include/v8config.h
@@ -1,4 +1,4 @@
-x// Copyright 2013 the V8 project authors. All rights reserved.
+// Copyright 2013 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
