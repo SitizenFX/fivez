181fbcf8aad461ba5fe10e3ef0ad4d67a9109966
Shi-Lan <XdllQs@gmail.com>
2025-10-15 19:21:24 +0200

    Update dspfilters.lua

--- code/vendor/dspfilters.lua
+++ code/vendor/dspfilters.lua
@@ -1,15 +1,1 @@
-return {
-	include = function()
-		includedirs { "../vendor/dspfilters/shared/DSPFilters/include/" }
-	end,
-	
-	run = function()
-		language "C++"
-		kind "StaticLib"
-		
-		files_project "../vendor/dspfilters/shared/DSPFilters/" {
-			"include/**.h",
-			"source/**.cpp",
-		}
-	end
-}
+
