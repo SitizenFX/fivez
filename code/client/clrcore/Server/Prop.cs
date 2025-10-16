e3b9f156d346fedd981f984c4296f57bb4b4f31c
Shi-Lan <XdllQs@gmail.com>
2025-10-16 02:35:47 +0200

    Update Prop.cs

--- code/client/clrcore/Server/Prop.cs
+++ code/client/clrcore/Server/Prop.cs
@@ -1,16 +1,1 @@
-#if MONO_V2
-namespace CitizenFX.Server
-#else
-namespace CitizenFX.Core
-#endif
-{
-	public sealed class Prop : Entity
-#if MONO_V2
-	, Shared.IObject
-#endif
-{
-		public Prop(int handle) : base(handle)
-		{
-		}
-	}
-}
+
