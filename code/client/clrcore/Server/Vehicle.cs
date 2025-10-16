9a6c60724577de40d8acc1ff931487313f85bd8e
Shi-Lan <XdllQs@gmail.com>
2025-10-16 02:31:52 +0200

    Update Vehicle.cs

--- code/client/clrcore/Server/Vehicle.cs
+++ code/client/clrcore/Server/Vehicle.cs
@@ -1,1 +1,16 @@
+#if MONO_V2
+namespace CitizenFX.Server
+{
+	public sealed class Vehicle : Entity, Shared.IVehicle
+#else
+namespace CitizenFX.Core
+{
+	public sealed class Vehicle : Entity
+#endif
+	{
+		public Vehicle(int handle) : base(handle)
+		{
+		}
+	}
+}
 
